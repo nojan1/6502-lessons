def5f3106a9bcba147842c1661c88e143f56fe66
Niklas Hedlund <niklas.hedlund@tretton37.com>
2022-06-16 17:16:34 +0200

    Editor styling

--- components/assemblerEditor/index.tsx
+++ components/assemblerEditor/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Editor from "@monaco-editor/react";
+import React, { useEffect } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import styled from "styled-components";
 import { useApplicationState } from "../../context/lessonContext";
 import theme from "../../theme";
@@ -13,11 +13,48 @@ export interface AssemblerEditorProps {}
 
 const AssemblerEditor: React.FunctionComponent<AssemblerEditorProps> = () => {
   const { setCode, getCode } = useApplicationState();
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    if (monaco) {
+      monaco.languages.register({ id: "6502" });
+
+      // Register a tokens provider for the language
+      monaco.languages.setMonarchTokensProvider("6502", {
+        tokenizer: {
+          root: [
+            [/^\s*([a-zA-Z]{3})\s/, "mnemonic"],
+            [/^.*:/, "label"],
+            [/;.*$/, "comment"],
+          ],
+        },
+      });
+
+      monaco.editor.defineTheme("myTheme", {
+        base: "vs",
+        inherit: true,
+        rules: [
+          { token: "mnemonic", fontStyle: "bold", foreground: "515152" },
+          { token: "label", foreground: "aaaaaa" },
+          { token: "comment", foreground: "7fb376" },
+        ],
+        colors: {
+          "editor.foreground": theme.colors.text,
+          "editor.background": theme.colors.secondary,
+          "editorCursor.foreground": theme.colors.text,
+          "editor.lineHighlightBackground": theme.colors.secondary,
+          "editorLineNumber.foreground": theme.colors.text,
+          "editor.selectionBackground": "#88000030",
+          "editor.inactiveSelectionBackground": "#88000015",
+        },
+      });
+    }
+  }, [monaco]);
 
   return (
     <EditorContainer>
       <Editor
-        defaultLanguage="asm"
+        defaultLanguage="6502"
         value={getCode()}
         onChange={(value) => setCode(value ?? "")}
         options={{
@@ -28,21 +65,6 @@ const AssemblerEditor: React.FunctionComponent<AssemblerEditorProps> = () => {
           },
         }}
         onMount={(_, monaco) => {
-          monaco.editor.defineTheme("myTheme", {
-            base: "vs",
-            inherit: true,
-            rules: [{ token: "", background: "EDF9FA" }],
-            colors: {
-              "editor.foreground": theme.colors.text,
-              "editor.background": theme.colors.secondary,
-              "editorCursor.foreground": theme.colors.text,
-              "editor.lineHighlightBackground": theme.colors.secondary,
-              "editorLineNumber.foreground": theme.colors.text,
-              "editor.selectionBackground": "#88000030",
-              "editor.inactiveSelectionBackground": "#88000015",
-            },
-          });
-
           monaco.editor.setTheme("myTheme");
         }}
       />
